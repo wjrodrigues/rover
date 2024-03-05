c302a6aa1ac2e83fb9d11759b7c0b0214dde7b49
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-05 15:54:25 -0300

    chore: change logic move rover

--- app/models/vehicle/rover.rb
+++ app/models/vehicle/rover.rb
@@ -4,31 +4,50 @@ module Vehicle
   class Rover
     include ActiveModel::Validations
 
-    attr_accessor :x_axis, :y_axis, :path
+    attr_accessor :x_axis, :y_axis, :path, :orientation
     private :path=
+
+    NORTH = 'N'
+    SOUTH = 'S'
+    EAST = 'E'
+    WEST = 'W'
 
     validates :x_axis, :y_axis, numericality: { only_integer: true, greater_than: 0 }
 
-    def initialize(x_axis: 0, y_axis: 0)
-      [x_axis, y_axis] => Integer, Integer
+    def initialize(x_axis: 0, y_axis: 0, orientation: NORTH)
+      [x_axis, y_axis, orientation] => Integer, Integer, String
 
       self.x_axis = x_axis
       self.y_axis = y_axis
+      self.orientation = orientation
       self.path = []
     end
 
     def position = { x_axis:, y_axis: }
 
-    def move_x_axis
-      self.x_axis = x_axis + 1
-
-      add_path
+    def move_left
+      return self.orientation = WEST if orientation == NORTH
+      return self.orientation = SOUTH if orientation == WEST
+      return self.orientation = EAST if orientation == SOUTH
+      return self.orientation = NORTH if orientation == EAST
 
       self
     end
 
-    def move_y_axis
-      self.y_axis = y_axis + 1
+    def move_right
+      return self.orientation = EAST if orientation == NORTH
+      return self.orientation = SOUTH if orientation == EAST
+      return self.orientation = WEST if orientation == SOUTH
+      return self.orientation = NORTH if orientation == WEST
+
+      self
+    end
+
+    def move
+      move_axis!
+
+      self.x_axis = 0 if x_axis.negative?
+      self.y_axis = 0 if y_axis.negative?
 
       add_path
 
@@ -37,6 +56,15 @@ module Vehicle
 
     private
 
-    def add_path = path << { x_axis:, y_axis: }
+    def move_axis!
+      return self.y_axis += 1 if orientation == NORTH
+      return self.y_axis -= 1 if orientation == SOUTH
+      return self.x_axis += 1 if orientation == EAST
+      return self.x_axis -= 1 if orientation == WEST
+
+      self
+    end
+
+    def add_path = path << { x_axis:, y_axis:, orientation: }
   end
 end
