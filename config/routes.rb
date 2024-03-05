d6344db713c8735a227997bff025db3e39a1079b
Washington Rodrigues <washingtonjunio16@gmail.com>
2024-03-05 08:37:45 -0300

    chore: create endpoint /api/v1/vehicle/rovers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      namespace :vehicle do
+        resources :rovers, only: [:create]
+      end
+    end
+  end
+
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
 end
